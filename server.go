b4cbd289fea66a562638c521c47e275a4c002840
gbenjamin <benjamin.gottrant@ynov.com>
2024-04-10 15:14:27 +0200

    one ws up

--- server.go
+++ server.go
@@ -5,9 +5,11 @@ import (
 	"html/template"
 	"log"
 	"net/http"
-	"strconv"
+
+	Groupi"Groupi/Groupi"
 	
 )
+
 
 func Home(w http.ResponseWriter, r *http.Request) {
 	tmpl, err := template.ParseFiles("./index.html")
@@ -18,29 +20,39 @@ func Home(w http.ResponseWriter, r *http.Request) {
 	tmpl.Execute(w, nil)
 }
 
-func GoBlindTest(w http.ResponseWriter, r *http.Request) {
+func GoBlindTest(w http.ResponseWriter, r *http.Request , str string) {
 	tmpl, err := template.ParseFiles("./pages/blindTest.html")
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	tmpl.Execute(w, nil)
+	// tab := []string{"Bonjour", "mon", "ami"}
+	// if str!="" {
+		
+	// 	tab = append(tab, str)
+	// }
+	tmpl.Execute(w,nil)
 }
 
 
-func main()  {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		Home(w, r)
-	})
-	http.HandleFunc("/goBlindTest", func(w http.ResponseWriter, r *http.Request) {
-		GoBlindTest(w, r)
-	})
-	http.HandleFunc("/blindTest", func(w http.ResponseWriter, r *http.Request) {
-		BlindTest(w, r)
-	})
-}
 
-fs := http.FileServer(http.Dir("static/"))
-	http.Handle("/static/", http.StripPrefix("/static/", fs))
-	fmt.Println("Server running on port 8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+func main() {
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        Home(w, r)
+    })
+
+    // URL pour le rendu de la page du blind test
+    http.HandleFunc("/goBlindTest", func(w http.ResponseWriter, r *http.Request) {
+        GoBlindTest(w, r, "")
+    })
+
+  
+    http.HandleFunc("/goBlindTest/webs", Groupi.WsBlindTest)
+
+    // Serveur de fichiers statiques
+    fs := http.FileServer(http.Dir("static/"))
+    http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+    fmt.Println("Server running on port 8080")
+    log.Fatal(http.ListenAndServe(":8080", nil))
+}
