e8bcfe537a7a0cdc88a999b27338c05ac38a9a8b
Bernard <romeo.bernard@ynov.com>
2024-04-10 16:58:18 +0200

    change local host in the real host

--- server.go
+++ server.go
@@ -1,4 +1,4 @@
-package main 
+package main
 
 import (
 	"fmt"
@@ -6,10 +6,8 @@ import (
 	"log"
 	"net/http"
 
-	Groupi"Groupi/Groupi"
-	
+	Groupi "Groupi/Groupi"
 )
-
 
 func Home(w http.ResponseWriter, r *http.Request) {
 	tmpl, err := template.ParseFiles("./index.html")
@@ -28,12 +26,11 @@ func GoBlindTest(w http.ResponseWriter, r *http.Request) {
 	}
 	// tab := []string{"Bonjour", "mon", "ami"}
 	// if str!="" {
-		
+
 	// 	tab = append(tab, str)
 	// }
-	tmpl.Execute(w,nil)
+	tmpl.Execute(w, nil)
 }
-
 
 func GoGuessTheSong(w http.ResponseWriter, r *http.Request) {
 	tmpl, err := template.ParseFiles("./pages/guessTheSong.html")
@@ -41,34 +38,30 @@ func GoGuessTheSong(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	tmpl.Execute(w,nil)
+	tmpl.Execute(w, nil)
 }
 
-
-
-
 func main() {
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        Home(w, r)
-    })
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		Home(w, r)
+	})
 
-    // URL pour le rendu de la page du blind test
-    http.HandleFunc("/goBlindTest", func(w http.ResponseWriter, r *http.Request) {
-        GoBlindTest(w, r)
-    })
+	// URL pour le rendu de la page du blind test
+	http.HandleFunc("/goBlindTest", func(w http.ResponseWriter, r *http.Request) {
+		GoBlindTest(w, r)
+	})
 
 	http.HandleFunc("/goGuessTheSong", func(w http.ResponseWriter, r *http.Request) {
-        GoGuessTheSong(w, r)
-    })
-  
-    http.HandleFunc("/goBlindTest/webs", Groupi.WsBlindTest)
+		GoGuessTheSong(w, r)
+	})
+
+	http.HandleFunc("/goBlindTest/webs", Groupi.WsBlindTest)
 	http.HandleFunc("/goGuessTheSong/webs", Groupi.WsGuessTheSong)
 
+	// Serveur de fichiers statiques
+	fs := http.FileServer(http.Dir("static/"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
 
-    // Serveur de fichiers statiques
-    fs := http.FileServer(http.Dir("static/"))
-    http.Handle("/static/", http.StripPrefix("/static/", fs))
-
-    fmt.Println("Server running on port 8080")
-    log.Fatal(http.ListenAndServe(":8080", nil))
+	fmt.Println("Server running on port 8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
